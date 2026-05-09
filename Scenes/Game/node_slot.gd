3cafb1a04de5de1fb18bca576d87a17fdf0d25ca
Seasnakez <adde456789@gmail.com>
2026-05-10 01:27:34 +0200

    Working multi type flow

--- Scenes/Game/node_slot.gd
+++ Scenes/Game/node_slot.gd
@@ -11,23 +11,25 @@ extends Node2D
 @export var powered_vision_amount = 0
 @export var powered_compute_amount = 0
 @export var powered_motion_amount = 0
+@export var unpowered_vision_amount = 0
+@export var unpowered_compute_amount = 0
+@export var unpowered_motion_amount = 0
 
-var power_amount = base_power_amount
-var vision_amount = powered_vision_amount
-var compute_amount = powered_compute_amount
-var motion_amount = powered_motion_amount
-
-var amount_arr = [power_amount, vision_amount, compute_amount, motion_amount]
+var amount_arr = []
 
 var connection : Connection = null
 var parent_node : SchematicsNode = null
 
 func _ready():
-	if dir == ResourceTypes.DIR.OUT and type == ResourceTypes.RT.POWER:
-		power_amount = 1
+	for a in ResourceTypes.RT.size():
+		amount_arr.append(0)
+	amount_arr[ResourceTypes.RT.POWER] = base_power_amount
+	amount_arr[ResourceTypes.RT.VISION] = 0
+	amount_arr[ResourceTypes.RT.COMPUTE] = 0
+	amount_arr[ResourceTypes.RT.MOTION] = 0
 	
 func _process(delta: float) -> void:
-	$Label.text = "P: " + str(power_amount) + "\nV: " + str(vision_amount) + "\nC: " + str(compute_amount) + "\nM: " + str(motion_amount)
+	$Label.text = "P: " + str(amount_arr[0]) + "\nV: " + str(amount_arr[1]) + "\nC: " + str(amount_arr[2]) + "\nM: " + str(amount_arr[3])
 
 func _on_slot_entered():
 	SignalBus.slot_entered.emit(self)
