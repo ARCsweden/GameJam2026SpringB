668933509a026e8e571a2d0642ef32d3bbb28f2c
Seasnakez <adde456789@gmail.com>
2026-05-09 18:04:10 +0200

    flow start

--- Scenes/Game/flow_manager.gd
+++ Scenes/Game/flow_manager.gd
@@ -11,5 +11,13 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_connection_made():
-	print("Connection made!")
+func _on_connection_made(start_slot: NodeSlot, stop_slot: NodeSlot):
+	# Add the following to each node: Input product, output product. 
+	# If no power: 0 output.
+	# When input or power state changes, update output. Then start going through all nodes connected
+	# to the output and update those in turn.
+	
+	for c in start_slot.parent_node.slots.get_children():
+		var ns : NodeSlot = c as NodeSlot
+		print(c.dir, c.type)
+	
--- Scripts/SignalBus.gd
+++ Scripts/SignalBus.gd
@@ -7,4 +7,4 @@ signal click_cancel()
 signal slot_entered(slot: NodeSlot)
 signal slot_exited(slot: NodeSlot)
 
-signal connection_made()
+signal connection_made(start_slot: NodeSlot, end_slot: NodeSlot)
