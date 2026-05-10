535599935c654f39315cc0ca5dff2d2f9a894637
Seasnakez <adde456789@gmail.com>
2026-05-10 11:35:24 +0200

    Added labels to connection

--- Scripts/connection.gd
+++ Scripts/connection.gd
@@ -2,6 +2,7 @@ class_name Connection
 extends Node
 
 var line : Line2D = null
+var label: Label = null
 var start : NodeSlot = null
 var end : NodeSlot = null
 var connection_type : ResourceTypes.RT
@@ -10,6 +11,11 @@ func update_line() -> void:
 	if line and start and end:
 		line.points[0] = start.global_position
 		line.points[1] = end.global_position
+		var between = line.points[0].lerp(line.points[1], 0.5)
+		label.text = "P:" + str(start.amount_arr[0]) + " V: " + str(start.amount_arr[1]) + " C: " + str(start.amount_arr[2]) + " M: " + str(start.amount_arr[3])
+		between.x -= label.size.x / 2
+		between.x -= label.size.y / 2
+		label.set_position(between)
 
 func remove() -> void:
 	if start:
