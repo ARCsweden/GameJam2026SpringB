f1fc105d1e18fa9eba54b11ac67d58abb93f3d7d
Rasmus Johansson <rasmussalo91@gmail.com>
2026-05-10 14:59:35 +0200

    Update connection_manager.gd

--- Scenes/Game/connection_manager.gd
+++ Scenes/Game/connection_manager.gd
@@ -4,12 +4,8 @@ const SFX_NODE_PRESS := "node_press"
 const SFX_CONNECTION_VALID := "connection_valid"
 const SFX_CONNECTION_INVALID := "connection_invalid"
 const SFX_CONNECTION_CANCELLED := "connection_cancelled"
-var label: Label = null
-var line : Line2D = null
-var start : NodeSlot = null
-var end : NodeSlot = null
-var connection_type : ResourceTypes.RT
 
+var label: Label = null
 var line: Line2D = null
 var start: NodeSlot = null
 var end: NodeSlot = null
@@ -17,10 +13,12 @@ var connection_type: ResourceTypes.RT
 
 var connections: Array[Connection] = []
 
+
 func _ready() -> void:
 	SignalBus.slot_entered.connect(_on_slot_entered)
 	SignalBus.slot_exited.connect(_on_slot_exited)
 	SignalBus.connection_removed.connect(_on_connection_removed)
+
 
 func _process(_delta: float) -> void:
 	for c in connections:
@@ -33,7 +31,7 @@ func _process(_delta: float) -> void:
 			line.points[1] = end.global_position
 		else:
 			line.points[1] = get_global_mouse_position()
-	# Update position of label
+
 	if label:
 		label.set_position(get_global_mouse_position())
 
@@ -50,6 +48,9 @@ func _unhandled_input(event: InputEvent) -> void:
 func _start_connection() -> void:
 	request_sfx(SFX_NODE_PRESS)
 
+	label = Label.new()
+	# label.text = str(start.amount_arr)
+
 	line = Line2D.new()
 
 	if start.connection:
@@ -64,12 +65,14 @@ func _start_connection() -> void:
 
 		line.add_point(get_global_mouse_position())
 
+		SignalBus.disconnect_slot_flow.emit(conn.start, conn.end)
 		_on_connection_removed(conn)
 	else:
 		line.add_point(start.global_position)
 		line.add_point(get_global_mouse_position())
 
 	add_child(line)
+	add_child(label)
 
 
 func _finish_connection() -> void:
@@ -79,6 +82,7 @@ func _finish_connection() -> void:
 		if _is_conn_valid(start, end):
 			var connection: Connection = Connection.new()
 			connection.line = line
+			connection.label = label
 			connection.start = start
 			connection.end = end
 
@@ -97,80 +101,31 @@ func _finish_connection() -> void:
 		request_sfx(SFX_CONNECTION_CANCELLED)
 
 	if not connection_created:
-		remove_child(line)
-		line.queue_free()
+		_discard_current_line()
+		_discard_current_label()
 
 	line = null
+	label = null
 	start = end
 	end = null
 
-			if line: # Finish connection
-				# Add connection to list if the line is valid
-				if end and _is_conn_valid(start, end):
-					var connection = Connection.new()
-					connection.line = line
-					connection.start = start
-					connection.end = end
-					connection.label = label
-					start.connection = connection
-					end.connection = connection
-					connections.append(connection)
-					SignalBus.update_slot_flow.emit(start, end)
-				# Clear old line/start/end
-				else:
-					remove_child(line)
-					line.queue_free()
-					remove_child(label)
-					label.queue_free()
-				line = null
-				label = null
-				start = end
-				end = null
-			elif start: # Create new connection
-				
-				# Create new label
-				label = Label.new()
-				#label.text = str(start.amount_arr)
-				
-				# Create line
-				line = Line2D.new()
-				
-				# Remove existing line
-				if start.connection:
-					# Make the other side of the existing connection the start
-					var conn = start.connection
-					if conn.start == start:
-						start = conn.end
-						line.add_point(conn.end.global_position)
-					else:
-						start = conn.start
-						line.add_point(conn.start.global_position)
-					line.add_point(get_global_mouse_position())
-					# Cleanup old line/connection
-					SignalBus.disconnect_slot_flow.emit(conn.start, conn.end)
-					_on_connection_removed(conn)
-					
-					
-				else:
-					# New line, starting at start node
-					line.add_point(start.global_position)
-					line.add_point(get_global_mouse_position())
-				add_child(line)
-				add_child(label)
 
 func _is_conn_valid(n1: NodeSlot, n2: NodeSlot) -> bool:
 	return n1.type == n2.type and n1.dir != n2.dir and n1.connection == null and n2.connection == null
 
 
 func _on_connection_removed(conn: Connection) -> void:
-	if conn:
-		if conn.line:
-			remove_child(conn.line)
+	if conn == null:
+		return
 
-		if conn.label:
-			remove_child(conn.label)
-		connections.erase(conn)
-		conn.remove()
+	if conn.line:
+		_remove_node_safely(conn.line)
+
+	if conn.label:
+		_remove_node_safely(conn.label)
+
+	connections.erase(conn)
+	conn.remove()
 
 
 func _on_slot_entered(node: NodeSlot) -> void:
@@ -195,3 +150,27 @@ func _on_slot_exited(_node: NodeSlot) -> void:
 
 func request_sfx(sound_name: String) -> void:
 	SignalBus.audio_sfx_requested.emit(sound_name)
+
+
+func _discard_current_line() -> void:
+	if line == null:
+		return
+
+	_remove_node_safely(line)
+
+
+func _discard_current_label() -> void:
+	if label == null:
+		return
+
+	_remove_node_safely(label)
+
+
+func _remove_node_safely(node: Node) -> void:
+	if node == null:
+		return
+
+	if node.get_parent():
+		node.get_parent().remove_child(node)
+
+	node.queue_free()
