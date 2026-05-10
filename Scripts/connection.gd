e08205ae56cd25f6f3550be1538ff56bfbbaf899
Seasnakez <adde456789@gmail.com>
2026-05-10 11:47:28 +0200

    Refined labels to be prettier

--- Scripts/connection.gd
+++ Scripts/connection.gd
@@ -12,7 +12,7 @@ func update_line() -> void:
 		line.points[0] = start.global_position
 		line.points[1] = end.global_position
 		var between = line.points[0].lerp(line.points[1], 0.5)
-		label.text = "P:" + str(start.amount_arr[0]) + " V: " + str(start.amount_arr[1]) + " C: " + str(start.amount_arr[2]) + " M: " + str(start.amount_arr[3])
+		set_label_text()
 		between.x -= label.size.x / 2
 		between.x -= label.size.y / 2
 		label.set_position(between)
@@ -23,3 +23,17 @@ func remove() -> void:
 	if end:
 		end.connection = null
 	line.queue_free()
+	
+func set_label_text():
+	var text = ""
+	if start.amount_arr[ResourceTypes.RT.POWER] != 0:
+		text += "P:" + str(start.amount_arr[ResourceTypes.RT.POWER]) + " "
+	if start.amount_arr[ResourceTypes.RT.VISION] != 0:
+		text += "V:" + str(start.amount_arr[ResourceTypes.RT.VISION]) + " "
+	if start.amount_arr[ResourceTypes.RT.COMPUTE] != 0:
+		text += "C:" + str(start.amount_arr[ResourceTypes.RT.COMPUTE]) + " "
+	if start.amount_arr[ResourceTypes.RT.MOTION] != 0:
+		text += "M:" + str(start.amount_arr[ResourceTypes.RT.MOTION]) + " "
+	label.text = text
+
+	
