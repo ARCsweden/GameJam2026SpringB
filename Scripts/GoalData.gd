c4b85fa72742a636abb43653338b661044097ee4
Per Lenander <per.lenander@wires.se>
2026-05-30 19:50:14 +0200

    Removed unused variables (commented out)

--- Scripts/GoalData.gd
+++ Scripts/GoalData.gd
@@ -15,11 +15,3 @@ enum LogicType {ACTIVE, PASSIVE}
 @export_multiline var goal_text: String
 @export var logic_type: LogicType = LogicType.PASSIVE
 @export var requirements: Array[GoalRequirement]
-#@export var amount_of_CPU: int
-#@export var amount_of_GPU: int
-#@export var amount_of_cameras: int # e.g., "Connect 5 pipes" -> target_value = 5
-#@export var camera_resolution_target: int
-#@export var camera_speed_target: int
-#@export var amount_of_arms: int
-#@export var amount_of_legs: int
-#@export var amount_of_monitors: int
